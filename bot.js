8753d6433844ccec0f71fa290692ca5b38364ef5
kumbulali <55637477+kumbulali@users.noreply.github.com>
2020-04-30 16:51:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,6 +61,10 @@ client.on('message', async message => {
           console.log(results);
 	  con.end();
 	  cooldown.add(message.author.id);
+	  setTimeout(() =>{
+	    cooldown.delete(message.author.id)
+	    }, cdseconds * 1000)
+	  });
           const skyblockmsg = new Discord.MessageEmbed()
             .setTitle("LignumCraft Ödül BOT")
             .setDescription(
