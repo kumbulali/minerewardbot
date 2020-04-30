982c0afdd9259d07893aa2938aa83d7b4f7b746e
kumbulali <55637477+kumbulali@users.noreply.github.com>
2020-04-30 16:37:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 let prefix = `!`;
 let cooldown = new Set();
-let cdseconds = 43200;
+let cdseconds = 10;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
